4e830d06dad6769091423cfdaa3b9be71d72b0f9
Mahmudjon Sayfiddinov <mahmudjon.sayfiddinov@gmail.com>
2026-03-20 00:46:20 +0500

    save for start new edits

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,11 +1,38 @@
 from django import forms
+# Импортируем класс ошибки валидации.
+from django.core.exceptions import ValidationError
+
+from .models import Birthday
+
+# Множество с именами участников Ливерпульской четвёрки.
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 
-class BirthdayForm(forms.Form):
-    """Form for collecting birthday information."""
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = Birthday
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(
+                format='%Y-%m-%d',
+                attrs={'type': 'date'}
+            )
+        }
 
-    first_name = forms.CharField(max_length=20, label='Имя')
-    last_name = forms.CharField(required=False, label='Фамилия',
-                                help_text='Необязательное поле')
-    birthday = forms.DateField(label='Дата рождения',
-                               widget=forms.DateInput(attrs={'type': 'date'}))
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
+
+    def clean(self):
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+# Импортируем валидатор для проверки возраста.
+from .validators import real_age
+
+class Birthday(models.Model):
+    first_name = models.CharField('Имя', max_length=20)
+    last_name = models.CharField(
+        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+    )
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', blank=True)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
